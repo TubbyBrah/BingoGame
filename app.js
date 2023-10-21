b58657fe860c807168497b36ee61de6dd2a1756a
TubbyBrah <michael.t.tsotsos@gmail.com>
2023-10-21 21:29:35 +1100

    new game reload feature
    added a start new game feature once all 90 numbers have been called

--- app.js
+++ app.js
@@ -18,7 +18,30 @@ function callNumber() {
   
   // Display previous numbers called
   previousNumbersElement.textContent = "Previous Numbers Called: " + numbersCalled.join(", ");
+
+
+// Check if all numbers have been called
+if (numbersCalled.length === 90) {
+  alert("Bingo! All 90 numbers have been called. Click the start new game button to start a new game");
+
+  // Enable the new game button
+  // Enable the new game button and display it
+  var newGameButton = document.getElementById("new-game-button");
+  newGameButton.style.display = "block";
 }
+}
+
+// Add an event listener to the new game button
+var newGameButton = document.getElementById("new-game-button");
+newGameButton.addEventListener("click", function() {
+// Reload the page to start a new game
+location.reload();
+});
+
+// Initially hide the new game button
+newGameButton.style.display = "none";
+
+
 
 // function startNewGame() {
 //     numbersCalled = [];
